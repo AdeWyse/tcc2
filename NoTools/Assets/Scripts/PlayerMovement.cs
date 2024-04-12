769e918dd4cb0dd0d5076235e4677faaa08be32a
AdeWYyse <adelinewyse@gmail.com>
2024-04-12 19:51:05 -0300

    R03 Player in bounds

--- NoTools/Assets/Scripts/PlayerMovement.cs
+++ NoTools/Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,24 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     public float movingSpeed = 5f;
 
+    private float screenWidth;
+    private float screenHeigth;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        screenWidth =  Camera.main.orthographicSize * Camera.main.aspect;
+        screenHeigth = Camera.main.orthographicSize;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        KeepInBounds();
     }
 
     private void MovePlayer()
@@ -27,6 +34,32 @@ public class PlayerMovement : MonoBehaviour
         if(inputHorizontal != 0 || inputVertical != 0)
         {
             rb.velocity = new Vector2(inputHorizontal, inputVertical) * movingSpeed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void KeepInBounds()
+    {
+        float verticalPosition = gameObject.transform.position.y;
+        float horizontalPosition = gameObject.transform.position.x;
+        if(verticalPosition < -screenHeigth + 0.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -screenHeigth + 0.5f, transform.position.z);
+        }
+        if ( verticalPosition > screenHeigth - 0.5f)
+        {
+            transform.position = new Vector3(transform.position.x, screenHeigth - 0.5f, transform.position.z);
+        }
+        if (horizontalPosition < -screenWidth + 0.5f)
+        {
+            transform.position = new Vector3(-screenWidth + 0.5f, transform.position.y, transform.position.z);
+        }
+        if (horizontalPosition > 0)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
         }
     }
 }
